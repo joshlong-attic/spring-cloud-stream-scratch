9f961506a891d07ccf1a1eaf966b65e86ae753d0
Josh Long <starbuxman@gmail.com>
2018-03-08 19:17:26 +0530

    actuator

--- producer-and-reactive-consumer/examples/consumer/src/main/kotlin/com/example/consumer/ConsumerApplication.kt
+++ producer-and-reactive-consumer/examples/consumer/src/main/kotlin/com/example/consumer/ConsumerApplication.kt
@@ -3,30 +3,21 @@ package com.example.consumer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.stream.annotation.EnableBinding
+import org.springframework.cloud.stream.annotation.Input
 import org.springframework.cloud.stream.annotation.StreamListener
 import org.springframework.cloud.stream.messaging.Sink
-import org.springframework.context.annotation.Bean
-import org.springframework.kafka.core.KafkaTemplate
+import reactor.core.publisher.Flux
 
 @SpringBootApplication
 @EnableBinding(Sink::class)
 class ConsumerApplication {
 
-/*
 	@StreamListener
-//	@SendTo(Source.OUTPUT)
-	fun incoming(@Input(Sink.INPUT) messages: Flux<String>)   {
+	fun incoming(@Input(Sink.INPUT) messages: Flux<String>) {
 		messages
 				.map { it.toUpperCase() }
 				.subscribe { println("received $it") }
 	}
-*/
-
-	@StreamListener(Sink.INPUT)
-	fun incoming(msg: String) {
-
-	}
-
 }
 
 fun main(args: Array<String>) {
